572ed471b319656aa9a92de12e1ee13a642b3429
rogerch93 <rogerinhochaves@hotmail.com>
2021-10-07 10:43:37 -0300

    alteração visual e logica de validar cpf front-end

--- Estacionamento MVC/Models/Propietario.cs
+++ Estacionamento MVC/Models/Propietario.cs
@@ -1,35 +1,52 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Estacionamento_MVC.Models
 {
     public class Propietario
     {
         public int Id { get; set; }
+        [Required]
         public string Modelo { get; set; }
+
+        [Required]
         public string Cor { get; set; }
+
+        [Required]
         [StringLength(100)]
         public string Marca { get; set; }
+
+        [Required]
         [StringLength(8)]
         public string Placa { get; set; }
 
+        [Required]
         [Display(Name = "Proprietário")]
         [StringLength(250)]
         public string NomePessoa { get; set; }
+
         [Required]
         [StringLength(11)]
         public string CPF { get; set; }
+
+        [Required]
         [Display(Name ="Vaga")]
         public int NumVaga { get; set; }
+
+        [Required]
         [DataType(DataType.Date)]
         public string Data { get; set; }
+
+        [Required]
         [Display(Name = "Entrada")]
         [DataType(DataType.Time)]
         public string HoraEntrada { get; set; }
+
+        [Required]
         [DataType(DataType.Time)]
         [Display(Name ="Saida")]
         public string HoraSaida { get; set; }
-        
+
+        [Required]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
         public decimal Total { get; set; }
         public bool Pago { get; set; }
---
+++ Estacionamento MVC/wwwroot/js/calc.js
@@ -0,0 +1,8 @@
+﻿$('#Calc').click(function () {
+    let ent = parseFloat(document.getElementById('Entrada').value);
+    let sai = parseFloat(document.getElementById('Saida').value);
+    let soma = ent + sai;
+    let total = soma * 5 - 100;
+    alert("O valor consumido foi de R$" + total + ",00");
+    
+})
